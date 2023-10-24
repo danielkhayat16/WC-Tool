ec9f680e82bf09bcad0b56155032f30d712ba005
Daniel Khayat <daniel.khayat8@gmail.com>
2023-10-24 23:56:20 +0300

    -l option allows to count lines

--- wc-tool.py
+++ wc-tool.py
@@ -1,42 +1,50 @@
 import os.path
 
+def checkInput(string):
+
+    commandValueError = string[0] != "ccwc"
+    optionValueError = string[1] != "-c" and string[1] != "-l" and string[1] != "-w" and string[1] != string[-1]
+    readFileError = not os.path.isfile(string[-1])
+    if(commandValueError):
+        print("Wrong input try again")
+    elif(optionValueError):
+        print("Wrong option, try again")
+    elif(readFileError):
+        print("No such file exist")
+    else: 
+        checkArgument(string)
+        
+        
+
+def checkArgument(string):
+    match string[1]:
+        case "-c":
+            print(f'{(os.path.getsize( string[-1]))} {string[-1]}')
+        case "-l":
+            
+            with(open(string[-1], "r") as f):
+                print(f"{len(f.readlines())} {string[-1]}")
+                f.close()
+        case "-w":
+            with(open(string[-1], "r") as f):
+                print(f"{len(f.readlines())} {string[-1]}")
+                f.close()
+            
+
 def main():
     print("This challenge is to build your own version of the Unix command line tool wc!")
-    str = ""
+    
     commandValueError = True
     optionValueError = True
     readFileError = True
     while( commandValueError or optionValueError or readFileError):
         try:
-            string = input('To use the command, please enter "ccwc -c <filename>"\n')
-            commandValueError = string[0:4] != "ccwc"
-            optionValueError = string[4:7] != " -c"
-            
-
-            readFileError = not os.path.isfile(string[8:])
-            if(commandValueError):
-                print("Wrong input try again")
-            elif(optionValueError):
-                print("Wrong argument, try again")
-            elif(readFileError):
-                print("No such file exist")
-            else:
-                print(f'{(os.path.getsize( string[8:]))} {string[8:]}')
+            string = input('To use the command, please enter "ccwc -c <filename>"\n').split()
+            checkInput(string)
+            break
                 
-
-
-
-
         except ValueError:
             print("Please enter a valid value")
-
-        
-    
-        # if(str[5:7] != "-c"):
-            
-        #     print("Wrong parameter")
-        #     str = ""
-
 
 if __name__ == "__main__":
     main()
